e4ea6c4feb57dfca1b3e76fed8fc61aa2bbef353
matheshme <133367307+matheshme@users.noreply.github.com>
2024-05-01 10:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-# app.py
+# # app.py
 
-from flask import Flask 
-from urllib.parse import quote 
+# from flask import Flask 
+# from urllib.parse import quote 
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
 
-    @app.route('/')
-    def home():
-        return 'Final Testing'
+#     @app.route('/')
+#     def home():
+#         return 'Final Testing'
 
-    return app
+#     return app
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(host='0.0.0.0', port=80, debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=80, debug=True)
-
+    app.run(debug=True)
